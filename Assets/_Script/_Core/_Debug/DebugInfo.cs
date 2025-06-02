a954a24527793edf479c2f310bd5d7885eec3f44
zhdltl601 <zhdltl601@gmail.com>
2025-06-02 11:27:23 +0900

    IMGUIdebug class complete

--- Assets/_Script/_Core/_Debug/DebugInfo.cs
+++ Assets/_Script/_Core/_Debug/DebugInfo.cs
@@ -1,18 +1,20 @@
+using System;
 using UnityEngine;
 
-internal struct DebugWorldInfo
+public struct DebugInfo
 {
     public Vector3 worldPosition;
+    internal bool is3D;
     public string message;
     public float sizeRatio;
 
-    public DebugWorldInfo(Vector3 worldPosition, string message, float sizeRatio = 1)
+    public DebugInfo(Vector3 worldPosition, string message, float sizeRatio = 1)
     {
         this.worldPosition = worldPosition;
         this.message = message;
         this.sizeRatio = sizeRatio;
+        is3D = false;
     }
-
     public readonly Vector3 GetScreenPos(Camera camera)
     {
         Vector3 result = camera.WorldToScreenPoint(worldPosition, Camera.MonoOrStereoscopicEye.Mono);
--- Assets/_Script/_Core/_Debug/IMGUIMono.cs
+++ Assets/_Script/_Core/_Debug/IMGUIMono.cs
@@ -1,60 +1,129 @@
+using System;
 using System.Collections.Generic;
 using UnityEngine;
+
+[Flags]
 public enum DebugKey
 {
-    none,
-    temp1,
-    temp2,
-    temp3
+    none = 0,
+    defaultFlag = 1 << 0,
+    temp2 = 1 << 1,
+    temp3 = 1 << 2,
+    temp4 = 1 << 3,
+    temp5 = 1 << 4
 }
 [DefaultExecutionOrder(-200)]
 public class IMGUIMono : MonoSingleton<IMGUIMono>
 {
     protected override MonoSingletonFlags SingletonFlag => MonoSingletonFlags.DontDestroyOnLoad;
-    public static DebugKey debugKey = DebugKey.none;
 
+    public static DebugKey CurrentDebugKey { get; set; } = DebugKey.defaultFlag;
     private static bool EnableGUI { get; set; } = true;
+    private static readonly List<DebugInfo> debugInfoList = new List<DebugInfo>(32);
 
-    private static readonly GUIStyle gUIStyle = new GUIStyle();
-    private static readonly GUIStyle style = gUIStyle;
-    private static readonly List<DebugWorldInfo> worldInfoList = new List<DebugWorldInfo>(32);
+    [SerializeField, Range(0, 1)] private float screenMultiplier = 1;
+    [SerializeField] private GUISkin debugGuiSkin;
 
-    [SerializeField] private float screenMultiplier = 0.03f;
-
-    protected override void Awake()
-    {
-        base.Awake();
-        style.alignment = TextAnchor.UpperLeft;
-    }
     private void Update()
     {
-        worldInfoList.Clear();
+        debugInfoList.Clear();
+        if (Input.GetKeyDown(KeyCode.F10))
+        {
+            EnableGUI = !EnableGUI;
+        }
     }
-    public static void DebugTextWorld<T>(T target, Vector3 worldPosition, float sizeRatio = 1, DebugKey key = DebugKey.none)
+    public static void DebugTextWorld(DebugInfo debugWorldInfo, DebugKey key = DebugKey.defaultFlag)
     {
-        if (debugKey != key) return;
+        if (!CanAddToList(key)) return;
 
-        DebugWorldInfo debugWorldInfo = new DebugWorldInfo(worldPosition, target.ToString(), sizeRatio);
-        worldInfoList.Add(debugWorldInfo);
+        debugWorldInfo.is3D = true;
+        debugInfoList.Add(debugWorldInfo);
+    }
+    public static void DebugTextWorld<T>(Vector3 worldPosition, T target, float sizeRatio = 1, DebugKey key = DebugKey.defaultFlag)
+    {
+        if (!CanAddToList(key)) return;
+
+        DebugInfo debugWorldInfo = new DebugInfo(worldPosition, target.ToString(), sizeRatio);
+        debugWorldInfo.is3D = true;
+        debugInfoList.Add(debugWorldInfo);
+    }
+    public static void DebugText(DebugInfo debugInfo, DebugKey key = DebugKey.defaultFlag)
+    {
+        if (!CanAddToList(key))
+        {
+            return;
+        }
+
+        debugInfoList.Add(debugInfo);
+    }
+    public static bool CanAddToList(DebugKey key)
+    {
+        bool result = (key & CurrentDebugKey) > 0 && EnableGUI;
+        return result;
     }
     private void OnGUI()
     {
         if (!EnableGUI) return;
+        Matrix4x4 originalGUIMatrix = GUI.matrix;
 
         Camera camera = Camera.main;
+
         int screenHeight = Screen.height;
-        int fontSize = (int)(screenHeight * screenMultiplier);
+        int screenWidth = Screen.width;
+        int fontSize = Mathf.Min(screenHeight, screenWidth);
 
-        foreach (DebugWorldInfo item in worldInfoList)
+        fontSize = (int)(fontSize * 0.005f * screenMultiplier);
+
+        Vector3 matrixScale = new Vector3(fontSize, fontSize, fontSize);
+        Matrix4x4 guiMatrixResolutionIndependent = Matrix4x4.Scale(matrixScale);
+        bool isZaxisZero = guiMatrixResolutionIndependent.m22 == 0;
+        if (isZaxisZero)
         {
-            style.fontSize = (int)(fontSize * item.sizeRatio);
-            Vector3 screenPosition = camera.WorldToScreenPoint(item.worldPosition, Camera.MonoOrStereoscopicEye.Mono);
-            screenPosition.y = screenHeight - screenPosition.y;
-
-            Vector2 size = new Vector3(2000, 2000);
-            Rect position = new Rect(screenPosition, size);
-
-            GUI.Label(position, item.message, style);
+            Debug.LogWarning($"adjust {nameof(screenMultiplier)}");
         }
+        GUI.matrix = guiMatrixResolutionIndependent;
+
+        int non3DCnt = 0;
+        int infoListCount = debugInfoList.Count;
+        for (int i = 0; i < infoListCount; i++)
+        {
+            DebugInfo item = debugInfoList[i];
+
+            Rect position;
+            Vector2 screenPosition;
+            Vector2 size;
+            string message = item.message;
+
+            const int k_charWidth = 10; //approx
+
+            size = new Vector3(item.message.Length * k_charWidth, 30);
+            if (item.is3D) //iterate World Text
+            {
+                screenPosition = camera.WorldToScreenPoint(item.worldPosition, Camera.MonoOrStereoscopicEye.Mono);
+                screenPosition.y = screenHeight - screenPosition.y;
+
+                screenPosition.x /= fontSize;
+                screenPosition.y /= fontSize;
+
+                position = new Rect(screenPosition, size);
+
+                GUI.Label(position, message, debugGuiSkin.label);
+            }
+            else //top left
+            {
+                const int k_charHeight = 30; //approx
+
+                screenPosition = new Vector2(10, non3DCnt * k_charHeight * fontSize);
+                screenPosition.x /= fontSize;
+                screenPosition.y /= fontSize;
+
+                position = new Rect(screenPosition, size);
+                GUI.Box(position, message, debugGuiSkin.box);
+                non3DCnt++;
+            }
+        }
+
+        //endl, set original GUI matrix
+        GUI.matrix = originalGUIMatrix;
     }
 }
