074142afef3b8ff9f946897e64be8a9e71f45e37
zhdltl601 <zhdltl601@gmail.com>
2025-06-02 17:20:44 +0900

    Removed Unused Scripts, Reposition

--- Assets/_Script/_Core/CustomSceneManager.cs
+++
@@ -1,9 +0,0 @@
-using UnityEngine;
-using UnityEngine.SceneManagement;
-public class CustomSceneManager
-{
-    //public async Awaitable LoadScene(LoadSceneMode loadSceneMode)
-    //{
-    //    //UnityEngine.SubsystemManager
-    //}
-}
